f5e8fc83f2da20defef770c593b236585b78dacb
prakriti <prakriti673@gmail.com>
2024-07-01 17:30:38 +0530

    create accountapi view added

--- bankapi/urls.py
+++ bankapi/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import BankAPIView, BranchAPIView, BranchDetailAPIView, BankDetailAPIView
-
+from .views import BankAPIView, BranchAPIView, BranchDetailAPIView, BankDetailAPIView, CreateAccountAPIView
 urlpatterns = [
     path('banks/', BankAPIView.as_view(), name='banks'),
     path('bank/<int:pk>/', BankDetailAPIView.as_view(), name='bank-detail'),
     path('branches/', BranchAPIView.as_view(), name='branches'),
     path('branch/<int:pk>/', BranchDetailAPIView.as_view(), name='branch-detail'),
+    path('create_account/',CreateAccountAPIView.as_view(), name='create-account'),
 ]
